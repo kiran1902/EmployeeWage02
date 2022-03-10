2b1c7316a45bb7d23477d589c4a394898ef2bca2
kiran1902 <kiru2kumar@gmail.com>
2022-03-10 13:44:35 +0530

    Employee Attendance

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welocome to Employee Wage");
+
+Random random = new Random();   //Generates Random Numbers
+
+const int Present = 1,
+            Absent = 0;
+
+int isPresent = random.Next(2);
+Console.WriteLine("isPresent" + isPresent);
+
+if(isPresent == 1)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
