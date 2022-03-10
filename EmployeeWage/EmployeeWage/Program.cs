de4ab1fdaa3500873e6f77e71629db03e26769e1
kiran1902 <kiru2kumar@gmail.com>
2022-03-10 14:12:09 +0530

    Calculating Employee Wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -4,16 +4,21 @@ Console.WriteLine("Welocome to Employee Wage");
 Random random = new Random();   //Generates Random Numbers
 
 const int Present = 1,
-            Absent = 0;
+            Absent = 0,
+            perHrWage=20,
+            Fulltime=8;
 
-int isPresent = random.Next(2);
+int isPresent = random.Next(2),
+    empWage = 0;
 Console.WriteLine("isPresent" + isPresent);
 
 if(isPresent == Present)
 {
     Console.WriteLine("Employee is Present");
+    empWage = perHrWage * Fulltime;
 }
 else
 {
     Console.WriteLine("Employee is Absent");
 }
+Console.WriteLine("empWage: " + empWage);
