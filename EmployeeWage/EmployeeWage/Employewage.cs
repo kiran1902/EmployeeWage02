813b2661d7b064f42b2ad4d76ca8bc742f40bbca
kiran1902 <kiru2kumar@gmail.com>
2022-03-16 21:59:19 +0530

    EmployeeWage problem using OOPs concept

---
+++ EmployeeWage/EmployeeWage/Employewage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Employewage
+    {
+        public void StartWageClac()
+        {
+            Console.WriteLine("Welocome to Employee Wage");
+
+            Random random = new Random();   //Generates Random Numbers
+
+            const int Present = 1,
+                        Halfday = 2,
+                        fullMonth = 3,
+                        Absent = 0,
+                        Fulltime = 8,
+                        Partime = 4,
+                        perHrWage = 20,
+                        perMonth = 20;
+
+            int isPresent = random.Next(4),    // Present = 1, Halfday = 2, fullMonth=3, Absent = 0,
+            empWage = 0;
+            Console.WriteLine("isPresent" + isPresent);
+
+
+
+
+            switch (isPresent)
+            {
+                case Present:
+                    Console.WriteLine("Employee is Present");
+                    empWage = perHrWage * Fulltime;
+                    break;
+
+                case Halfday:
+                    Console.WriteLine("Employee is Present");
+                    empWage = perHrWage * Partime;
+                    break;
+
+                case fullMonth:
+                    Console.WriteLine("Employee Monthly salary");
+                    empWage = perMonth * (Fulltime * perHrWage);
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+
+            }
+            /*       if (isPresent == Present)
+                   {
+                       Console.WriteLine("Employee is Present");
+                       empWage = perHrWage * Fulltime;
+                   }
+                   else if (isPresent == Halfday)
+                   {
+                       Console.WriteLine("Employee is Present");
+                       empWage = perHrWage * Partime;
+                   }
+                   else
+                   {
+                       Console.WriteLine("Employee is Absent");
+                   }
+
+           */
+
+            Console.WriteLine("empWage: " + empWage);
+        }
+    }
+}
