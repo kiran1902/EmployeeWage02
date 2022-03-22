1419b06db324ceb9b5eba39254e53713f0957aa7
kiran1902 <kiru2kumar@gmail.com>
2022-03-22 15:05:17 +0530

    Multiple Companies

---
+++ EmployeeWage/EmployeeWage/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class Company
+    {
+        int totalEmpWage = 0;
+        int numOfEmp = 30;
+
+        public int EmpWageCalc()
+        {
+            for (int i = 0; i < numOfEmp; i++)
+            {
+                Employewage emp = new Employewage();
+                totalEmpWage += emp.StartWageClac();
+
+            }
+            return totalEmpWage;
+        }
+    }
+
+   
+}
